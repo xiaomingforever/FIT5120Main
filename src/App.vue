16f4d1829d34e221632ae5c6aedee6b4a849952c
yzha1104 <yzha1104@gmail.com>
2025-09-04 23:49:08 +1000

    fix some bugs

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router"
+
+const route = useRoute()
+</script>
 
 <template><router-view :key="$route.fullPath" /></template>
 <style scoped></style>
